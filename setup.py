9232812bdb758af25384256abfbe6d77b80e33b9
Todd Bryan <todd.r.bryan@gmail.com>
2014-08-15 15:03:19 -0700

    Still getting setup right

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='parabolapy',
+      version='0.1',
+      description='Parabola Geoprocessing Server Utilities',
+      author='McClintock Lab, UCSB',
+      author_email='admin@seasketch.org',
+      url='https://github.com/mcclintock-lab/parabolapy',
+      packages=['parabolapy', 'parabolapy.parabolapy'],
+     )
