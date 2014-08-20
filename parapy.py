ad04c3870a1ae6d88c8077ce431a3393fac4f0c0
Todd Bryan <todd.r.bryan@gmail.com>
2014-08-20 13:40:46 -0700

    FLesh out API some; parse incoming JSON

--- parapy.py
+++ parapy.py
@@ -1,3 +1,32 @@
-def touch():
-  print "ParaPy is loaded!"
+import sys
+import jsonlib
+
+
+_input = ''
+
+
+def getInputArgumentNames():
+  this_input = _getInputJson()
+  return(this_input.keys())
+
+def getInputArgumentByName(name):
+  this_input = _getInputJson()
+  return(this_input[name])
+
+def getRawInput():
+  return(_getInputJson())
+
+def setOutput(code, message, output): 
+  print("setOUtput")
+
+def _getInputJson():
+  global _input
+  if len(_input) > 0:
+    return(_input)
+  this_input = sys.stdin.read()
+  _input = jsonlib.read(this_input)
+  return(_input)
+
+
+
 
--- parapy/parapy.py
+++
@@ -1,3 +0,0 @@
-def touch():
-  print "ParaPy is loaded!"
-
