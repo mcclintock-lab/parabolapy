fa82c8f62f4074b473408bec4af9893faa7cb052
Todd Bryan <todd.r.bryan@gmail.com>
2014-08-21 10:31:55 -0700

    Change API again

--- parapy.py
+++ parapy.py
@@ -1,23 +1,37 @@
-import sys
+import sys, os
 import jsonlib
 
 
-_input = ''
-
+_input = '';
+_output = {};
 
 def getInputArgumentNames():
   this_input = _getInputJson()
   return(this_input.keys())
 
-def getInputArgumentByName(name):
+def getInputArgumentAsObject(name):
   this_input = _getInputJson()
   return(this_input[name])
 
-def getRawInput():
+def getInputArgumentAsGeoJson(name):
+  this_input = _getInputJson()
+  return(jsonlib.write(this_input[name]))
+
+def getInputAsObject():
   return(_getInputJson())
 
-def setOutput(code, message, output): 
-  print("setOUtput")
+def getInputAsGeoJson():
+  return(jsonlib.write(getInputAsObject()));
+
+def setOutputByName(name, geojson): 
+  global _output;
+  _output[name] = geojson;
+
+def sendOutput():
+  global _output;
+  outfile = os.fdopen(3, 'w');
+  outfile.write(_output);
+  outfile.close();
 
 def _getInputJson():
   global _input
@@ -26,7 +40,4 @@ def _getInputJson():
   this_input = sys.stdin.read()
   _input = jsonlib.read(this_input)
   return(_input)
-
-
-
 
